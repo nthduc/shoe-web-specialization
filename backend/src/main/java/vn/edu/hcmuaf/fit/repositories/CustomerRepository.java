5ebf190b6c2c0b0d5baf6a8f0804757738fb442d
nthduc <nguyenthaiduc0212@gmail.com>
2023-02-25 07:59:31 +0700

    :hammer: repository Customer(Khách hàng)

--- backend/src/main/java/vn/edu/hcmuaf/fit/repositories/CustomerRepository.java
+++ backend/src/main/java/vn/edu/hcmuaf/fit/repositories/CustomerRepository.java
@@ -3,9 +3,10 @@ package vn.edu.hcmuaf.fit.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import vn.edu.hcmuaf.fit.models.Customer;
+/*Interface này kế thừa JpaRepository để thực hiện các thao tác CRUD trên đối tượng Customer. */
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Customer findByEmail(String email);
-    Customer findById(Long idCustomer) ;
+    Customer findByEmail(String email); // tìm kiếm và trả về đối tượng Customer theo địa chỉ email của khách hàng.
+    Customer findById(Long idCustomer) ; // tìm kiếm và trả về đối tượng Customer theo ID của khách hàng.
 }
