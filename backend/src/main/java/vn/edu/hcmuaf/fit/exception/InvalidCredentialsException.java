0b23153b0065d8710edac0e8167a87c3b5c64ca3
nthduc <nguyenthaiduc0212@gmail.com>
2023-03-01 16:17:06 +0700

    :hammer: kiểm tra email tồn tại trong DB and login , ngoại lệ

---
+++ backend/src/main/java/vn/edu/hcmuaf/fit/exception/InvalidCredentialsException.java
@@ -0,0 +1,7 @@
+package vn.edu.hcmuaf.fit.exception;
+
+public class InvalidCredentialsException extends RuntimeException {
+    public InvalidCredentialsException(String message) {
+        super(message);
+    }
+}
--- backend/src/main/java/vn/edu/hcmuaf/fit/services/CustomerService.java
+++ backend/src/main/java/vn/edu/hcmuaf/fit/services/CustomerService.java
@@ -1,21 +1,24 @@
 package vn.edu.hcmuaf.fit.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import vn.edu.hcmuaf.fit.exception.InvalidCredentialsException;
 import vn.edu.hcmuaf.fit.exception.ResourceNotFoundException;
 import vn.edu.hcmuaf.fit.models.Customer;
 import vn.edu.hcmuaf.fit.repositories.CustomerRepository;
 
 import java.util.List;
-
+import java.util.Optional;
+/* sử dụng Optional để bọc kết quả trả về từ phương thức , đồng thời tránh được tình trạng null pointer exception nếu kết quả trả về là null.*/
 @Service
 public class CustomerService {
 
     @Autowired
     private CustomerRepository customerRepository;
 
-    // Lưu thông tin một khách hàng mới (thêm khách hàng) vào CSDL
+    // Lưu thông tin một khách hàng mới (thêm khách hàng), mã hóa mật khẩu và lưu vào CSDL
     public Customer saveCustomer(Customer customer) {
         BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
         String encodedPassword = encoder.encode(customer.getPassword());
@@ -52,6 +55,41 @@ public class CustomerService {
         existingCustomer.setAddress(customer.getAddress());
 
         return customerRepository.save(existingCustomer);
+    }
+
+    // kiểm tra Email đã tồn tại trong CSDL hay chưa
+    /*
+    khởi tạo một đối tượng Optional bao bọc một đối tượng Customer,
+    trả về bởi phương thức findByEmail trong interface CustomerRepository.
+    Phương thức findByEmail được sử dụng để truy vấn một khách hàng dựa trên địa chỉ email.
+    Nếu khách hàng tồn tại, phương thức sẽ trả về đối tượng Customer, ngược lại sẽ trả về null.
+    */
+    public boolean isExistEmail(String email) {
+        Optional<Customer> customer = Optional.ofNullable(customerRepository.findByEmail(email));
+        return customer.isPresent();
+    }
+
+    /*
+    Đây là method để xác thực đăng nhập của khách hàng.
+    Phương thức này nhận vào hai tham số email và password của khách hàng.
+    Sau đó, phương thức sẽ tìm kiếm khách hàng với email được cung cấp trong cơ sở dữ liệu.
+    Nếu khách hàng không tồn tại, phương thức sẽ ném ra một ngoại lệ ResourceNotFoundException.
+    Nếu khách hàng tồn tại, phương thức sẽ so sánh mật khẩu được cung cấp với mật khẩu của khách hàng.
+    Nếu mật khẩu không khớp, phương thức sẽ ném ra một ngoại lệ InvalidCredentialsException.
+    Nếu mật khẩu khớp, phương thức sẽ trả về khách hàng được xác thực.
+    */
+    public Customer login(String email, String password) throws InvalidCredentialsException {
+        Optional<Customer> customer = Optional.ofNullable(customerRepository.findByEmail(email));
+        if (customer.isPresent()) {
+            String hashedPassword = customer.get().getPassword();
+            if (BCrypt.checkpw(password, hashedPassword)) {
+                return customer.get();
+            } else {
+                throw new InvalidCredentialsException("Invalid email or password");
+            }
+        } else {
+            throw new InvalidCredentialsException("Invalid email or password");
+        }
     }
 
 }
